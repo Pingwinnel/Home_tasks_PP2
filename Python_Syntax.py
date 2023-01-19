8b67761d2e4838a148ad395967a59d4a7da416ff
Erkiller <erasil123gns@gmail.com>
2023-01-19 23:08:38 +0600

    tasks

---
+++ Python_Syntax.py
@@ -0,0 +1,10 @@
+#If else (block syntax with tabulation)
+if(100>10*10):
+    print("")
+elif(100>90):
+    if(100>99):
+        print("100 greater than 99")
+    print("100 is greater than 90")
+    
+#This is comment syntax
+    
