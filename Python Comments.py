5c19fbbc86f1400b751e2eb9adb91028461dd52a
Erkiller <erasil123gns@gmail.com>
2023-01-19 23:22:20 +0600

    tasks

---
+++ Python Comments.py
@@ -0,0 +1,19 @@
+#This is a comment with #
+print("Hello world")
+# print("Hello World")
+
+#you can use multiple line comments 
+#if you add for 
+#each line #
+
+"""""
+
+or You can use string beacause
+python doesnt make attention to 
+string without variables
+and for comments number of 
+string symbols should to 
+be a odd number for each sides
+
+
+"""
