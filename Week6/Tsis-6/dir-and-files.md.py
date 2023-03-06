e9e28670c9e82d2d12af8d324271f4c836e821c6
Erkiller <erasil123gns@gmail.com>
2023-03-07 00:25:27 +0600

    ex_week6

--- Week6/Tsis-6/dir-and-files.md.py
+++ Week6/Tsis-6/dir-and-files.md.py
@@ -36,3 +36,20 @@ for i in range(26):
         f=open(f"{char}.txt","x")
     except IOError as error:
         pass
+#7ex
+path1="A.txt"
+path2="B.txt"
+f=open(path1,"r")
+f1=open(path2,"w")
+f1.write(f.read())
+#8ex
+path3="C.txt"
+print('Exist:', os.access(path, os.F_OK))
+print('Readable:', os.access(path, os.R_OK))
+print('Writable:', os.access(path, os.W_OK))
+print('Executable:', os.access(path, os.X_OK))
+if os.access(path, os.F_OK)==True:
+    os.remove(path3)
+    print("File succesful deleted")
+else:
+    print("no such file in directory")
