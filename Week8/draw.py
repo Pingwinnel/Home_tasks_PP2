972509407f5b15130884a907d1fb98a8b0521ee4
Erkiller <erasil123gns@gmail.com>
2023-04-01 23:28:32 +0600

    ex_week8

--- Week8/draw.py
+++ Week8/draw.py
@@ -1,7 +1,10 @@
 import pygame 
 import random
 
-
+CARS_SIZE=[100,100]
+SIZE=(400,600)
+screen=pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Car Game")
 
 
 
@@ -14,3 +17,15 @@ while True:
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
+        elif pygame.event==pygame.KEYDOWN:
+            if event.key==pygame.K_LEFT:
+                x_change=-10
+            elif event.key==pygame.K_RIGHT:
+                x_change=10
+            elif event.key==pygame.K_UP:
+                y_change=-10
+            elif event.key==pygame.K_DOWN:
+                y_change=10
+            
+    screen.fill("WHITE")
+    pygame.display.update()
